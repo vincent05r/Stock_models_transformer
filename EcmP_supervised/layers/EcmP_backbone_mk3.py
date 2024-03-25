97d57c0f6a2a89cd8807e57e872f7e61a4bb1984
vincent05r <111105725+vincent05r@users.noreply.github.com>
2024-03-25 14:29:01 +1100

    command updates

--- EcmP_supervised/layers/EcmP_backbone_mk3.py
+++ EcmP_supervised/layers/EcmP_backbone_mk3.py
@@ -112,13 +112,13 @@ class Flatten_Head_EcmP(nn.Module):
         # if self.individual:
         #     self.linears = nn.ModuleList()
         #     self.dropouts = nn.ModuleList()
-        #     self.flattens = nn.ModuleList()
+        #     self.Ecmp_flattens = nn.ModuleList()
         #     for i in range(self.n_vars):
-        #         self.flattens.append(nn.Flatten(start_dim=-2))
+        #         self.Ecmp_flattens.append(nn.Flatten(start_dim=-2))
         #         self.linears.append(nn.Linear(nf, target_window))
         #         self.dropouts.append(nn.Dropout(head_dropout))
         # else:
-        #     self.flatten = nn.Flatten(start_dim=-2)
+        #     self.Ecmp_flatten = nn.Flatten(start_dim=-2)
         #     self.linear = nn.Linear(nf, target_window)
         #     self.dropout = nn.Dropout(head_dropout)
             
@@ -128,13 +128,13 @@ class Flatten_Head_EcmP(nn.Module):
         # if self.individual:
         #     x_out = []
         #     for i in range(self.n_vars):
-        #         z = self.flattens[i](x[:,i,:,:])          # z: [bs x d_model * patch_num]
+        #         z = self.Ecmp_flattens[i](x[:,i,:,:])          # z: [bs x d_model * patch_num]
         #         z = self.linears[i](z)                    # z: [bs x target_window]
         #         z = self.dropouts[i](z)
         #         x_out.append(z)
         #     x = torch.stack(x_out, dim=1)                 # x: [bs x nvars x target_window]
         # else:
-        #     x = self.flatten(x)
+        #     x = self.Ecmp_flatten(x)
         #     x = self.linear(x)
         #     x = self.dropout(x)
 
