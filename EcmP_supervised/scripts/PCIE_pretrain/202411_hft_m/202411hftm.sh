059e7750d84ae47ba251c1932697e8555ce2f735
Vincent Zhu <111105725+vincent05r@users.noreply.github.com>
2024-11-28 13:14:39 +1100

    script updates

--- EcmP_supervised/scripts/PCIE_pretrain/202411_hft_m/202411hftm.sh
+++ EcmP_supervised/scripts/PCIE_pretrain/202411_hft_m/202411hftm.sh
@@ -48,45 +48,47 @@ result_log_path=./result_log/PCIE/pretrain/2411_hftm.txt
 
 for pred_len in 1 2 10 20
 do
-    seq_len=360
-    python -u EcmP_supervised/run_pretrain_v2.py \
-    --wandb_project $wandb_project \
-    --freq 1T \
-    --model_load_path None \
-    --result_log_path $result_log_path \
-    --is_pretrain 1 \
-    --pe zeros \
-    --learn_pe True \
-    --decomposition $decomposition \
-    --kernel_size $kernel_size \
-    --first_stage_patching $first_stage_patching \
-    --second_stage_patching $second_stage_patching \
-    --random_seed $random_seed \
-    --root_path $root_path_name \
-    --model_id 'HFTM_PCIE_'$seq_len'_'$pred_len \
-    --model $model_name \
-    --data $data_name \
-    --features MS \
-    --seq_len $seq_len \
-    --label_len $label_len \
-    --pred_len $pred_len \
-    --scale $scale \
-    --target $target \
-    --dt_format_str $dt_format_str \
-    --enc_in 9 \
-    --e_layers 4 \
-    --n_heads 6 \
-    --d_patch 0 \
-    --d_model 216 \
-    --d_ff 512 \
-    --dropout 0 \
-    --fc_dropout 0 \
-    --head_dropout 0 \
-    --patch_len 24 \
-    --stride 1 \
-    --des 'Exp' \
-    --train_epochs 25 \
-    --lradj 'TST' \
-    --pct_start 0.1 \
-    --itr 1 --batch_size 128 --learning_rate 0.0001 >logs/PCIE/pretrain/$model_name'_HFTM_'$last_folder_name'_'$model_id_name'_'$seq_len'_'$pred_len.log 
+    for seq_len in 30 60 120
+    do
+        python -u EcmP_supervised/run_pretrain_v2.py \
+        --wandb_project $wandb_project \
+        --freq 1T \
+        --model_load_path None \
+        --result_log_path $result_log_path \
+        --is_pretrain 1 \
+        --pe zeros \
+        --learn_pe True \
+        --decomposition $decomposition \
+        --kernel_size $kernel_size \
+        --first_stage_patching $first_stage_patching \
+        --second_stage_patching $second_stage_patching \
+        --random_seed $random_seed \
+        --root_path $root_path_name \
+        --model_id 'HFTM_PCIE_'$seq_len'_'$pred_len \
+        --model $model_name \
+        --data $data_name \
+        --features MS \
+        --seq_len $seq_len \
+        --label_len $label_len \
+        --pred_len $pred_len \
+        --scale $scale \
+        --target $target \
+        --dt_format_str $dt_format_str \
+        --enc_in 9 \
+        --e_layers 3 \
+        --n_heads 6 \
+        --d_patch 0 \
+        --d_model 108 \
+        --d_ff 256 \
+        --dropout 0 \
+        --fc_dropout 0 \
+        --head_dropout 0 \
+        --patch_len 12 \
+        --stride 1 \
+        --des 'Exp' \
+        --train_epochs 25 \
+        --lradj 'TST' \
+        --pct_start 0.1 \
+        --itr 1 --batch_size 128 --learning_rate 0.0001 >logs/PCIE/pretrain/$model_name'_HFTM_'$last_folder_name'_'$model_id_name'_'$seq_len'_'$pred_len.log 
+    done
 done
